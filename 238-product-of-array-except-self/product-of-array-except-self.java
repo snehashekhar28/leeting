8525912ae0a5bb3f219f638ef6d2f979fb8a002d
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-02-09 05:43:56 -0600

    Time: 2 ms (94.19%) | Memory: 72.3 MB (26.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,29 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] preNums = new int[nums.length];
-        int[] postNums = new int[nums.length];
-        int[] output = new int[nums.length];
         for(int i = 0; i < nums.length; i++){
             preNums[i] = 1;
-            postNums[i] = 1;
         }
         int runningPre = 1;
         int runningPost = 1;
         for(int i = 1; i < nums.length; i++){
             runningPre *= nums[i-1];
             preNums[i] *= runningPre;
-            System.out.println("pre:" + i + " " + preNums[i]);
         }
         for(int i = nums.length-2; i >= 0; i--){
             runningPost *= nums[i+1];
-            postNums[i] *= runningPost;
-            System.out.println("post:" + i + " " + postNums[i]);
+            preNums[i] *= runningPost;
         }
-        // output[0] = runningPost[0];
-        // output[nums.length-1] = runningPre[nums.length-1];
-        for(int i = 0; i < output.length; i++){
-            output[i] = postNums[i] * preNums[i];
-        }
-        return output;
+    
+        return preNums;
     }
 }
