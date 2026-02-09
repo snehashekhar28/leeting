70b32a476f3514da2c9a3feaef8731292467cdc1
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-02-09 05:02:10 -0600

    Time: 73 ms (5.08%) | Memory: 50.4 MB (19.75%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,27 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //map:
-        //sorted chars : List of corresponding unsorted words
-        Map<String, List<String>> nmap = new HashMap<>();
-        List<List<String>> toRet = new ArrayList<>();
-        for(int i = 0; i < strs.length; i++){
-            char[] chArr = strs[i].toCharArray();
-            Arrays.sort(chArr);
-            String sorted = new String(chArr);
-            if(nmap.containsKey(sorted)){
-                nmap.get(sorted).add(strs[i]);
+        //return everything together that are made up of same core letters
+        //use hashmap to track core letters?
+        // 'nat' -> ['a','n','t']
+        // 'tan' -> ['a','n','t']
+        //^^ same, so put in one list...
+
+        // 'ant'/ ['a','n','t'] -> List (tan, nat)
+        Map <String , List<String>> anagramGroup = new HashMap<>();
+        for(String word: strs){
+            char[] wordArr = word.toCharArray();
+            Arrays.sort(wordArr);
+            System.out.println("1" + wordArr);
+            String wordStr = new String(wordArr);
+            System.out.println("2" + wordStr);
+            List<String> group = anagramGroup.get(wordStr);
+            if(group == null){
+                group = new ArrayList<>();
             }
-            else{
-                List<String> aList = new ArrayList<>();
-                aList.add(strs[i]);
-                nmap.put(sorted, aList);
-            }
+            group.add(word);
+            anagramGroup.put(wordStr,group);
+        }
+        List<List<String>> out = new ArrayList<>();
+        for(String anList : anagramGroup.keySet()){
+            out.add(anagramGroup.get(anList));
         }
 
-        for(String sortedKey: nmap.keySet()){
-            toRet.add(nmap.get(sortedKey));
-        }
-
-        return toRet;
-}
+        return out;
+    }
 }
