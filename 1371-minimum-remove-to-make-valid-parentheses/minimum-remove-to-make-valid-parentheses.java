a15d1bf5b8882fea5a91f4e7774896276167dc8f
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-02-09 07:10:09 -0600

    Time: 21 ms (50.05%) | Memory: 47.6 MB (31.20%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,35 +1,26 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        //char[] sArr = s.toCharArray();
-        Stack<Integer> parenStack = new Stack<>();
-        Set<Integer> incClose = new HashSet<>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '(') parenStack.push(i);
-            if(s.charAt(i) == ')'){
-                if(parenStack.isEmpty()) incClose.add(i);
-                else if(s.charAt(parenStack.peek()) == '('){
-                    parenStack.pop();
-                } else{
-                    incClose.add(i);
+        char[] sArr = s.toCharArray();
+        Stack<Integer> parenTrack = new Stack<>();
+        for(int i = 0; i < sArr.length; i++){
+            if(sArr[i] == '(') parenTrack.push(i);
+            if(sArr[i] == ')'){
+                if(parenTrack.isEmpty()) sArr[i] = 'N';
+                else if(sArr[parenTrack.peek()] == '('){
+                    parenTrack.pop();
+                } else {
+                    sArr[i] = 'N';
                 }
             }
         }
-        int i = s.length() - 1;
-        StringBuilder sb = new StringBuilder();
-        while(!parenStack.empty() || !incClose.isEmpty() || i >=0){
-            if(!parenStack.empty() && parenStack.peek() == i){
-                parenStack.pop();
-                i--;
-                continue;
-            } 
-            if(incClose.contains(i)){
-                incClose.remove(i);
-                i--;
-                continue;
-            }
-            sb.append(s.charAt(i));
-            i--;
+        while(!parenTrack.isEmpty()){
+            sArr[parenTrack.pop()] = 'N';
         }
-        return sb.reverse().toString();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < sArr.length; i++){
+            if(sArr[i] != 'N')
+            sb.append(sArr[i]);
+        }
+        return sb.toString();
     }
 }
