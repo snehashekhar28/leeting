280abd6d03b80d93fe97732fd2c654e93675cf6d
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-02-11 01:28:31 -0600

    Time: 22 ms (45.25%) | Memory: 47.8 MB (17.96%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,25 +1,20 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
+        Stack <Integer> parenStack = new Stack <>();
         char[] sArr = s.toCharArray();
-        Stack<Integer> parenTrack = new Stack<>();
-        for(int i = 0; i < sArr.length; i++){
-            if(sArr[i] == '(') parenTrack.push(i);
-            if(sArr[i] == ')'){
-                if(parenTrack.isEmpty()) sArr[i] = 'N';
-                else if(sArr[parenTrack.peek()] == '('){
-                    parenTrack.pop();
-                } else {
-                    sArr[i] = 'N';
-                }
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') parenStack.push(i);
+            else if(s.charAt(i) == ')'){
+                if(parenStack.isEmpty()) sArr[i] = '.';
+                else if(s.charAt(parenStack.peek()) == '(') parenStack.pop();
             }
         }
-        while(!parenTrack.isEmpty()){
-            sArr[parenTrack.pop()] = 'N';
+        while(!parenStack.isEmpty()){
+            sArr[parenStack.pop()] = '.';
         }
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < sArr.length; i++){
-            if(sArr[i] != 'N')
-            sb.append(sArr[i]);
+        for(char v : sArr){
+            if(v != '.') sb.append(v);
         }
         return sb.toString();
     }
