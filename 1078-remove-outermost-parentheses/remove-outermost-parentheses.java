1a03cba1125600473590c0657aa727eb742b976d
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-02-12 13:04:07 -0600

    Time: 2 ms (99.57%) | Memory: 43.6 MB (53.58%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,19 +1,16 @@
 class Solution {
     public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
         int depth = 0;
         char[] sArr = s.toCharArray();
         for(int i = 0; i < sArr.length; i++){
             if(sArr[i] == '('){
-                if(depth == 0) sArr[i] = '.';
+                if(depth != 0) sb.append(sArr[i]);
                 depth++;
             } else {
                 depth--;
-                if(depth == 0) sArr[i] = '.';
+                if(depth != 0) sb.append(sArr[i]);
             }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(char c : sArr){
-            if(c!= '.') sb.append(c);
         }
         return sb.toString();
     }
