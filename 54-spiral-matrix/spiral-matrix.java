7366547edf5176eb1ba536dd9f3a07288cf0d4a1
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-05-13 13:59:14 -0500

    Time: 4 ms (1.19%) | Memory: 42.9 MB (77.71%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> out = new ArrayList<>();
+        int i = 0, j = 0;
+        while(matrix[i][j] != -200){
+            if(j + 1 < matrix[0].length && matrix[i][j+1] != -200){
+                while(j + 1 < matrix[0].length && matrix[i][j+1] != -200){
+                    out.add(matrix[i][j]);
+                    System.out.println(matrix[i][j]);
+                    matrix[i][j] = -200;
+                    j++;
+                }
+            }
+            else if (i + 1 < matrix.length && matrix[i+1][j] != -200){
+                while (i + 1 < matrix.length && matrix[i+1][j] != -200){
+                    out.add(matrix[i][j]);
+                    System.out.println(matrix[i][j]);
+                    matrix[i][j] = -200;
+                    i++;
+                }
+            }
+            else if(j - 1 >= 0 && matrix[i][j-1] != -200){
+                while (j - 1 >= 0 && matrix[i][j-1] != -200){
+                    out.add(matrix[i][j]);
+                    System.out.println(matrix[i][j]);
+                    matrix[i][j] = -200;
+                    j--;
+                }
+            }
+            else if(i - 1 >= 0 && matrix[i-1][j] != -200){
+                while(i - 1 >= 0 && matrix[i-1][j] != -200){
+                    out.add(matrix[i][j]);
+                    System.out.println(matrix[i][j]);
+                    matrix[i][j] = -200;
+                    i--;
+                }
+            }
+            else{
+                break;
+            }
+        }
+        out.add(matrix[i][j]);
+        return out;
+    }
+}
