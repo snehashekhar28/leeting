57f65f64ac26789d5a5154e380567780f8a528e1
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-05-15 14:47:07 -0500

    Time: 3 ms (77.56%) | Memory: 47.8 MB (84.09%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -8,13 +8,11 @@ class Solution {
         int j = 0;
         while(im < rows * cols){
             diag[im] = mat[i][j];
-            System.out.println(diag[im]);
             im++;
             //going up diagonally
             if(i - 1 >= 0 && j + 1 < mat[0].length){
                 while(i - 1 >= 0 && j + 1 < mat[0].length){
                     diag[im] = mat[--i][++j];
-                    System.out.println(diag[im]);
                     im++;
                 }
                 if(j + 1 >= mat[0].length) i++; else j++;
@@ -23,7 +21,6 @@ class Solution {
             else if (i + 1 < mat.length && j - 1 >= 0){
                 while(i + 1 < mat.length && j - 1 >= 0){
                     diag[im] = mat[++i][--j];
-                    System.out.println(diag[im]);
                     im++;
                 }
                 if(i + 1 >= mat.length) j++; else i++;
