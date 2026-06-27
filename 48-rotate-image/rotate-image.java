5e2e663b7df4f814f62cdce9ae95e8f19dd4aa13
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-06-27 00:15:23 -0500

    Time: 0 ms (100.00%) | Memory: 43.5 MB (83.24%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,25 +1,15 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length-1;
-        int n1 = matrix.length-1;
-        int i = 0;
-        //int j = 0;
-        for(; i < n; i++){
-            // System.out.println(i);
-            // System.out.println(j);
-            // System.out.println(n);
-            // System.out.println();
-            int j = i;
-            for(; j < n; j++){
-                int p0 = matrix[j][n1-i];
-                int p1 = matrix[n1-i][n1-j];
-                int p2 = matrix[n1-j][i];
-                matrix[j][n1-i] = matrix[i][j];
-                matrix[n1-i][n1-j] = p0;
-                matrix[n1-j][i] = p1;
-                matrix[i][j] = p2;
+        for(int i = 0; i < matrix.length/2; i++){
+            for(int j = i; j < matrix[0].length - i - 1; j++){
+                int ij1 = matrix[i][j];
+                int ij2 = matrix[j][matrix.length - 1 - i];
+                int ij3 = matrix[matrix.length - 1 - i][matrix.length - 1 - j];
+                matrix[i][j] = matrix[matrix.length - 1 - j][i];
+                matrix[matrix.length - 1 - j][i] = ij3;
+                matrix[matrix.length - 1 - i][matrix.length - 1 - j] = ij2;
+                matrix[j][matrix.length - 1 - i] = ij1;
             }
-            n--;
         }
     }
 }
