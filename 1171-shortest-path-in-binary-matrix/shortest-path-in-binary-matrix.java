e346d5c39a1f9fe5cbc009d9e876aab69a56d825
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-06-27 03:29:51 -0500

    Time: 32 ms (10.42%) | Memory: 48 MB (12.54%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -12,35 +12,35 @@ class Solution {
                 int[] a = q.poll();
                 if(a[0] == n - 1 && a[1] == n - 1) return track;
                 if(a[0] + 1 < n && grid[a[0]+1][a[1]] == 0){
-                    q.offer(new int[]{a[0]+1,a[1]});
+                    q.add(new int[]{a[0]+1,a[1]});
                     grid[a[0]+1][a[1]] = 1;
                 }
                 if(a[0] - 1 >= 0 && grid[a[0]-1][a[1]] == 0){
-                    q.offer(new int[]{a[0]-1,a[1]});
+                    q.add(new int[]{a[0]-1,a[1]});
                     grid[a[0]-1][a[1]] = 1;
                 }
                 if(a[1] - 1 >= 0 && grid[a[0]][a[1]-1] == 0){
-                    q.offer(new int[]{a[0],a[1]-1});
+                    q.add(new int[]{a[0],a[1]-1});
                     grid[a[0]][a[1]-1] = 1;
                 }
                 if(a[1] + 1 < n && grid[a[0]][a[1]+ 1] == 0){
-                    q.offer(new int[]{a[0],a[1]+1});
+                    q.add(new int[]{a[0],a[1]+1});
                     grid[a[0]][a[1]+1] = 1;
                 }
                 if(a[1] + 1 < n && a[0] + 1 < n && grid[a[0]+1][a[1]+ 1] == 0){
-                    q.offer(new int[]{a[0]+1,a[1]+1});
+                    q.add(new int[]{a[0]+1,a[1]+1});
                     grid[a[0]+1][a[1]+1] = 1;
                 }
                 if(a[1] + 1 < n && a[0] - 1 >= 0 && grid[a[0]-1][a[1]+ 1] == 0){
-                    q.offer(new int[]{a[0]-1,a[1]+1});
+                    q.add(new int[]{a[0]-1,a[1]+1});
                     grid[a[0]-1][a[1]+1] = 1;
                 }
                 if(a[1] - 1 >= 0 && a[0] + 1 < n && grid[a[0]+1][a[1] - 1] == 0){
-                    q.offer(new int[]{a[0]+1,a[1]-1});
+                    q.add(new int[]{a[0]+1,a[1]-1});
                     grid[a[0]+1][a[1]-1] = 1;
                 }
                 if(a[1] - 1 >= 0 && a[0] - 1 >= 0 && grid[a[0]-1][a[1] - 1] == 0){
-                    q.offer(new int[]{a[0]-1,a[1]-1});
+                    q.add(new int[]{a[0]-1,a[1]-1});
                     grid[a[0]-1][a[1]-1] = 1;
                 }
             }
