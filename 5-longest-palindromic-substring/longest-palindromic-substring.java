f3eaba184e0b13c82fff349d364d4695d23fe85e
snehashekhar28 <71354208+snehashekhar28@users.noreply.github.com>
2026-07-02 01:22:52 -0500

    Time: 131 ms (24.23%) | Memory: 48.6 MB (5.55%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,30 @@
 class Solution {
     public String longestPalindrome(String s) {
-        boolean[][] lP = new boolean[s.length()][s.length()];
-        String maxS = "";
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int iBest = 0; int jBest = 0;
         for(int len = 1; len <= s.length(); len++){
-            for(int i = 0; i <= s.length() - len; i++){
-                int j = i + len - 1;
-                if(j - i == 0){
-                    lP[i][j] = true;
-                    if(maxS.length() < j - i + 1)
-                        maxS = s.substring(i,j + 1);
+            for(int left = 0; left <= s.length() - len; left++){
+                int right = left + len - 1;
+                if(len == 1){
+                    dp[left][right] = true;
                 }
-                if(j - i == 1 && s.charAt(i) == s.charAt(j)){
-                    lP[i][j] = true;
-                    if(maxS.length() < j - i + 1)
-                        maxS = s.substring(i,j + 1);
+                else if(len == 2 && s.charAt(left) == s.charAt(right)){
+                    dp[left][right] = true;
                 }
-                if(j-i >= 2 && s.charAt(i) == s.charAt(j) &&
-                    lP[i+1][j-1]){
-                    lP[i][j] = true;
-                    if(maxS.length() < j - i + 1)
-                        maxS = s.substring(i,j + 1);
+                else if(s.charAt(left) == s.charAt(right) && dp[left + 1][right - 1]){
+                    dp[left][right] = true;
                 }
+                else{
+                    continue;
+                }
+
+                iBest = left;
+                jBest = right + 1;
+                // 1 char = true (a)
+                // 2 char = true (bb)
+                // 3+ char = true ( //_x_ ### _x_)
             }
         }
-        return maxS;
-        
+       return s.substring(iBest, jBest);
     }
 }
